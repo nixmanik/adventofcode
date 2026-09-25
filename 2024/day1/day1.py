35e86818a8db1600ae6dc4e412c8c9a24dc8b0a2
nixmanik <niki@nixnerver.cc>
2026-09-25 17:33:53 +1000

    2024 day 1 part 2 complete

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 def get_data(filename):
     lista, listb = [], []
     with open(filename, 'r') as file:
@@ -13,16 +14,23 @@ def get_diffs(a, b):
     diffs = []
     for x,y in zip(a,b):
         diffs.append(abs(x-y))
-    return diffs
+    return sum(diffs)
 
-def print_solution(diffs):
-    print(sum(diffs))
+def get_sim_score(a, b):
+    seenb = Counter(b)
+    score = [num * seenb[num] for num in a]
+    return sum(score)
+
+def print_solution(diffs, simscore):
+    print(diffs)
+    print(simscore)
 
 def main():
     filename = 'day1.txt'
     a, b = get_data(filename)
     diffs = get_diffs(a,b)
-    print_solution(diffs)
+    score = get_sim_score(a,b)
+    print_solution(diffs,score)
 
 if __name__ == '__main__':
     main()
